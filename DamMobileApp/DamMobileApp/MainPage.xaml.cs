85662a86f041043b0f260bd9c0e5b79dd31a9339
Mikko Myllys <mikko.myllys@eng.tamk.fi>
2018-10-09 13:36:31 +0300

    Removed timePicker because datePicker is enough, also edited UI looks and changed the Plot X-Axis to be DateTime-Axis

--- DamMobileApp/DamMobileApp/MainPage.xaml.cs
+++ DamMobileApp/DamMobileApp/MainPage.xaml.cs
@@ -50,8 +50,12 @@ namespace DamMobileApp
             {
                 Title = PlotTitle
             };
+            var startDate = DateTime.Now.AddDays(-10);
+            var endDate = DateTime.Now;
+            var minValue = DateTimeAxis.ToDouble(startDate);
+            var maxValue = DateTimeAxis.ToDouble(endDate);
             model.Axes.Add(new LinearAxis() { Position = AxisPosition.Left, Minimum = 0, Maximum = 100, IsPanEnabled = false, IsZoomEnabled = false });
-            model.Axes.Add(new DateTimeAxis() { Position = AxisPosition.Bottom, Minimum = 0, Maximum = 100, IsPanEnabled = false, IsZoomEnabled = false });
+            model.Axes.Add(new DateTimeAxis() { Position = AxisPosition.Bottom, Minimum = minValue, Maximum = maxValue, IsPanEnabled = false, IsZoomEnabled = false });
 
             // Create alert line
             var alertLine = new LineSeries
@@ -59,19 +63,29 @@ namespace DamMobileApp
                 Color = OxyColor.Parse("#ff0000"),
                 Title = "Alert line"
             };
-            alertLine.Points.Add(new DataPoint(0, 70));
-            alertLine.Points.Add(new DataPoint(100, 70));
+            alertLine.Points.Add(new DataPoint(minValue, 70));
+            alertLine.Points.Add(new DataPoint(maxValue, 70));
 
             // Create series
             var series = new LineSeries
             {
                 Color = OxyColor.Parse("#0000ff")
             };
-            series.Points.Add(new DataPoint(0, 5));
-            series.Points.Add(new DataPoint(20, 90));
-            series.Points.Add(new DataPoint(40, 80));
-            series.Points.Add(new DataPoint(60, 20));
-            series.Points.Add(new DataPoint(80, 80));
+            var data1 = DateTime.Now.AddDays(-10);
+            var data2 = DateTime.Now.AddDays(-8);
+            var data3 = DateTime.Now.AddDays(-6);
+            var data4 = DateTime.Now.AddDays(-4);
+            var data5 = DateTime.Now.AddDays(-2);
+            var d1 = DateTimeAxis.ToDouble(data1);
+            var d2 = DateTimeAxis.ToDouble(data2);
+            var d3 = DateTimeAxis.ToDouble(data3);
+            var d4 = DateTimeAxis.ToDouble(data4);
+            var d5 = DateTimeAxis.ToDouble(data5);
+            series.Points.Add(new DataPoint(d1, 5));
+            series.Points.Add(new DataPoint(d2, 90));
+            series.Points.Add(new DataPoint(d3, 80));
+            series.Points.Add(new DataPoint(d4, 20));
+            series.Points.Add(new DataPoint(d5, 80));
 
             // Add series and alert line
             model.Series.Add(series);
